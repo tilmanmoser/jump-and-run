360bfc0eb1b68ac6aa49f0ca92f6be98a41ad277
Tilman Moser <tilman.moser@impavit.com>
2024-05-04 08:54:50 +0200

    Level Editor (with tons of assets)

--- editor.py
+++ editor.py
@@ -1,54 +1,84 @@
+import sys
 import pygame
+import pygame.gfxdraw
+
+from scripts.assets import load_tile_assets
+from scripts.tilemap import Tilemap
 
 INITIAL_DISPLAY_SIZE = [800, 500]
 FPS = 60
 
 
 class Editor:
-    def __init__(self):
+    def __init__(self, path="map.json"):
+        self.level_file = path
+
         # display
         pygame.init()
         pygame.display.set_caption("Level Editor")
         self.screen = pygame.display.set_mode(INITIAL_DISPLAY_SIZE, pygame.RESIZABLE)
         self.display = pygame.Surface(INITIAL_DISPLAY_SIZE)
         self.display_scale = 1
-
-        # loop
         self.clock = pygame.Clock()
 
-        # inputs
+        # user inputs & derived states
         self.movement = [False, False, False, False]
         self.shift = False
         self.click = [False, False]
+        self.ongrid = True
+        self.tile_type = 0
+        self.tile_variant = 0
+        self.scroll = [0, 0]
+
+        self.tile_assets = load_tile_assets()
+        self.tile_list = list(self.tile_assets)
+        self.tilemap = Tilemap(self.tile_assets)
+        try:
+            self.tilemap.load(self.level_file)
+        except FileNotFoundError:
+            pass
 
     def resize(self, size):
         # fixed height, variable width
         self.display_scale = INITIAL_DISPLAY_SIZE[1] / size[1]
         self.display = pygame.Surface((size[0] * self.display_scale, INITIAL_DISPLAY_SIZE[1]))
 
-    def scroll_up(self):
-        pass
-
-    def scroll_down(self):
-        pass
-
-    def update(self):
-        pass
-
-    def render(self):
-        # display
-        self.display.fill((0, 0, 0, 0))
-
-        # screen
-        self.screen.fill((0, 0, 0, 0))
-        self.screen.blit(
-            pygame.transform.scale(self.display, (self.display.get_width() / self.display_scale, self.display.get_height() / self.display_scale)), (0, 0)
-        )
-        pygame.display.update()
+    def update_tile(self, increment=1, variant=False):
+        if variant:
+            self.tile_variant = (self.tile_variant + increment) % len(self.tile_assets[self.tile_list[self.tile_type]])
+        else:
+            self.tile_type = (self.tile_type + increment) % len(self.tile_list)
+            self.tile_variant = 0
 
     def run(self):
         running = True
         while running:
+            # camera position
+            self.scroll[0] += (self.movement[1] - self.movement[0]) * 8
+            self.scroll[1] += (self.movement[3] - self.movement[2]) * 8
+            render_offset = (int(self.scroll[0]), int(self.scroll[1]))
+            # relative mouse position
+            mpos = pygame.mouse.get_pos()
+            mpos = (mpos[0] * self.display_scale, mpos[1] * self.display_scale)
+
+            # placing tiles ongrid (offgrid in events once per click)
+            if self.click[0] and self.ongrid:
+                self.tilemap.add((mpos[0] + render_offset[0], mpos[1] + render_offset[1]), self.tile_list[self.tile_type], self.tile_variant, self.ongrid)
+            # removing tiles
+            if self.click[1]:
+                self.tilemap.remove((mpos[0] + render_offset[0], mpos[1] + render_offset[1]), render_offset, self.ongrid)
+
+            # rendering
+            self.display.fill((0, 0, 0, 0))
+            self.render_background()
+            self.tilemap.render(self.display, render_offset)
+            self.render_current_tile(mpos)
+
+            self.screen.fill((0, 0, 0, 0))
+            self.screen.blit(
+                pygame.transform.scale(self.display, (self.display.get_width() / self.display_scale, self.display.get_height() / self.display_scale)), (0, 0)
+            )
+            pygame.display.update()
 
             # user inputs
             for event in pygame.event.get():
@@ -69,6 +99,12 @@ class Editor:
                         self.movement[3] = True
                     if event.key in (pygame.K_LSHIFT, pygame.K_RSHIFT):
                         self.shift = True
+                    if event.key == pygame.K_g:
+                        self.ongrid = not self.ongrid
+                    if event.key == pygame.K_o:
+                        self.tilemap.save(self.level_file)
+                    if event.key == pygame.K_t:
+                        self.tilemap.autotile()
                 if event.type == pygame.KEYUP:
                     if event.key in (pygame.K_LEFT, pygame.K_a):
                         self.movement[0] = False
@@ -83,22 +119,52 @@ class Editor:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         self.click[0] = True
+                        if not self.ongrid:
+                            self.tilemap.add(
+                                (mpos[0] + render_offset[0], mpos[1] + render_offset[1]), self.tile_list[self.tile_type], self.tile_variant, self.ongrid
+                            )
                     if event.button == 3:
                         self.click[1] = True
                     if event.button == 4:
-                        self.scroll_up()
+                        self.update_tile(increment=+1, variant=self.shift)
                     if event.button == 5:
-                        self.scroll_down()
+                        self.update_tile(increment=-1, variant=self.shift)
                 if event.type == pygame.MOUSEBUTTONUP:
                     if event.button == 1:
                         self.click[0] = False
                     if event.button == 3:
                         self.click[1] = False
 
-            self.update()
-            self.render()
             self.clock.tick(FPS)
+
+    def render_current_tile(self, mpos):
+        tile_img = self.tile_assets[self.tile_list[self.tile_type]][self.tile_variant]
+        if self.ongrid:
+            self.display.blit(
+                tile_img,
+                (
+                    ((mpos[0] + self.scroll[0]) // self.tilemap.tile_size) * self.tilemap.tile_size - self.scroll[0],
+                    ((mpos[1] + self.scroll[1]) // self.tilemap.tile_size) * self.tilemap.tile_size - self.scroll[1],
+                ),
+            )
+        else:
+            self.display.blit(tile_img, mpos)
+
+    def render_background(self):
+        pygame.gfxdraw.textured_polygon(
+            self.display,
+            [
+                (0, 0),
+                (self.display.get_width(), 0),
+                self.display.get_size(),
+                (0, self.display.get_height()),
+            ],
+            self.tile_assets["backgrounds"][self.tilemap.background],
+            self.tile_assets["backgrounds"][self.tilemap.background].get_width(),
+            self.tile_assets["backgrounds"][self.tilemap.background].get_height(),
+        )
 
 
 if __name__ == "__main__":
-    Editor().run()
+    file_path = sys.argv[1] if len(sys.argv) > 1 else "map.json"
+    Editor(file_path).run()
---
+++ scripts/tilemap.py
@@ -0,0 +1,129 @@
+import json
+
+import pygame
+
+AUTOTILE_MAP = {
+    # tiles are created in a 4x4 grid;
+    # numbering is done from 00 (top left) to 15 (bottom right)
+    #
+    # .---.---.---.---.
+    # | a | a | a | c |  a = area
+    # +---+---+---+---+
+    # | a | a | a | c |  c = single column
+    # +---+---+---+---+
+    # | a | a | a | c |  r = single row
+    # +---+---+---+---+
+    # | r | r | r | s |  s = single block
+    # `---^---^---^---´
+    #
+    # areas
+    tuple(sorted([(1, 0), (0, 1)])): 0,  # top left
+    tuple(sorted([(1, 0), (-1, 0), (0, 1)])): 1,  # top center
+    tuple(sorted([(-1, 0), (0, 1)])): 2,  # top right
+    tuple(sorted([(1, 0), (0, 1), (0, -1)])): 4,  ## middle left
+    tuple(sorted([(1, 0), (-1, 0), (0, 1), (0, -1)])): 5,  ## middle
+    tuple(sorted([(-1, 0), (0, 1), (0, -1)])): 6,  ## middle right
+    tuple(sorted([(1, 0), (0, -1)])): 8,  # # bottom left
+    tuple(sorted([(1, 0), (-1, 0), (0, -1)])): 9,  # bottom middle
+    tuple(sorted([(-1, 0), (0, -1)])): 10,  # bottom right
+    # single row
+    tuple(sorted([(1, 0)])): 12,  # left
+    tuple(sorted([(1, 0), (-1, 0)])): 13,  # center
+    tuple(sorted([(-1, 0)])): 14,  # right
+    # single col
+    tuple(sorted([(0, 1)])): 3,  # top
+    tuple(sorted([(0, 1), (0, -1)])): 7,  # middle
+    tuple(sorted([(0, -1)])): 11,  # bottom
+    # single block
+    tuple(): 15,
+}
+
+
+class Tilemap:
+    def __init__(self, assets={}):
+        self.assets = assets
+        self.background = 0
+        self.tile_size = 16
+        self.tiles = {}
+        self.offgrid = []
+
+    def load(self, path):
+        f = open(path, "r")
+        data = json.load(f)
+        f.close()
+        self.tile_size = data["tile_size"]
+        self.tiles = data["tiles"]
+        self.offgrid = data["offgrid"]
+        self.background = data["background"]
+
+    def save(self, path):
+        f = open(path, "w")
+        json.dump({"background": self.background, "tile_size": self.tile_size, "tiles": self.tiles, "offgrid": self.offgrid}, f)
+        f.close()
+
+    def add(self, pos, t_type, variant, ongrid=True):
+        if t_type == "backgrounds":
+            self.background = variant
+        elif ongrid:
+            pos = (int(pos[0] // self.tile_size), int(pos[1] // self.tile_size))
+            self.tiles[str(pos[0]) + ";" + str(pos[1])] = {"type": t_type, "variant": variant, "pos": pos}
+        else:
+            self.offgrid.append({"type": t_type, "variant": variant, "pos": pos})
+
+    def remove(self, pos=(0, 0), offset=(0, 0), ongrid=True):
+        if ongrid:
+            tile_loc = str(int(pos[0] // self.tile_size)) + ";" + str(int(pos[1] // self.tile_size))
+            if tile_loc in self.tiles:
+                del self.tiles[tile_loc]
+        else:
+            for tile in self.offgrid.copy():
+                tile_img = self.assets[tile["type"]][tile["variant"]]
+                tile_rect = pygame.Rect(
+                    tile["pos"][0],
+                    tile["pos"][1],
+                    tile_img.get_width(),
+                    tile_img.get_height(),
+                )
+                if tile_rect.collidepoint(pos):
+                    self.offgrid.remove(tile)
+
+    def autotile(self):
+        for loc in self.tiles:
+            tile = self.tiles[loc]
+            if str(tile["type"]).startswith("tiles/"):
+                neighbors = set()
+                for shift in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                    check_loc = str(tile["pos"][0] + shift[0]) + ";" + str(tile["pos"][1] + shift[1])
+                    if check_loc in self.tiles:
+                        if self.tiles[check_loc]["type"] == tile["type"]:
+                            neighbors.add(shift)
+                neighbors = tuple(sorted(neighbors))
+                if neighbors in AUTOTILE_MAP:
+                    tile["variant"] = AUTOTILE_MAP[neighbors]
+
+    def render(self, surface: pygame.Surface, offset=(0, 0)):
+        for tile in self.offgrid:
+            surface.blit(
+                self.assets[tile["type"]][tile["variant"]],
+                (tile["pos"][0] - offset[0], tile["pos"][1] - offset[1]),
+            )
+
+        # only render visible ongrid tiles
+        for x in range(
+            offset[0] // self.tile_size,
+            (offset[0] + surface.get_width()) // self.tile_size + 1,
+        ):
+            for y in range(
+                offset[1] // self.tile_size,
+                (offset[1] + surface.get_height()) // self.tile_size + 1,
+            ):
+                loc = str(x) + ";" + str(y)
+                if loc in self.tiles:
+                    tile = self.tiles[loc]
+                    surface.blit(
+                        self.assets[tile["type"]][tile["variant"]],
+                        (
+                            tile["pos"][0] * self.tile_size - offset[0],
+                            tile["pos"][1] * self.tile_size - offset[1],
+                        ),
+                    )
